6a2de1f389ffbffbf36efe7f83c8f1ce0227173e
Patrick RAGUIN <patrick@patrick-raguin.fr>
2017-09-04 11:41:37 -0300

    Ajout commentaires

--- src/actions/AxiosExplorer.js
+++ src/actions/AxiosExplorer.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: 'http://localhost/reactexplorer',
+  baseURL: 'https://patrick-raguin.fr/reactexplorer/api',
   timeout: 2000,
   headers: {
     'Accept': 'application/json',
